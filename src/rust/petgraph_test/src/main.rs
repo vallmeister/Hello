c115e9fcc67ac053fbd120e6e3e51a42ceed5e07
Christian Valenti <christian.valenti@uni-ulm.de>
2022-11-07 12:40:34 +0100

    Rust recap, refactoring and grep :grapes: :recycle:

--- src/rust/petgraph_test/src/main.rs
+++
@@ -1,9 +0,0 @@
-// see https://depth-first.com/articles/2020/02/03/graphs-in-rust-an-introduction-to-petgraph/
-
-use petgraph::graph::Graph;
-
-fn main() {
-    let mut graph = Graph::<(), ()>::new();
-    graph.extend_with_edges(&[ (0, 1) ]);
-    assert_eq!(graph.node_count(), 2);
-}
