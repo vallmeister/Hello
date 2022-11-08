87e9c00b101d4c61bb5d6711e85cf7009327f5ed
Christian Valenti <christian.valenti@uni-ulm.de>
2022-11-08 13:33:14 +0100

    Improved IO project by iterators and closures :satellite: :closed_umbrella:

--- src/rust/mini_projects/minigrep/src/lib.rs
+++ src/rust/mini_projects/minigrep/src/lib.rs
@@ -9,16 +9,22 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 { 
-            return Err("not enough arguments");
-         }
+    pub fn build(
+        mut args: impl Iterator<Item = String>
+    ) -> Result<Config, &'static str> {
+        args.next();
 
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string")
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
-
         Ok(Config {query, file_path, ignore_case })
 
     }
@@ -52,6 +58,13 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
+pub fn search_functional<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
+}
+
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let mut results = Vec::new();
     let query = query.to_lowercase();
@@ -64,6 +77,13 @@ pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a st
     
     
     results
+}
+
+pub fn search_case_insensitive_functional<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(query))
+        .collect::<Vec<&'a str>>()
 }
 
 #[cfg(test)]
